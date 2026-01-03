7a4774e29d25f7c65927ffd90b1edaca8c585894
waw <anindyaagnes79@gmail.com>
2026-01-03 20:28:13 +0700

    upload decor png for css & routing product backend

---
+++ backend/models/productModels.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {type: String, require: true},
+    price: {type: Number, require: true},
+    description: {type: String, require: true},
+    image: {type: String, require: true},
+    category: {type: String, require: true},
+})
+
+const productModel = mongoose.models.produect || mongoose.model('product', productSchema)
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { addProduct, listProducts, removeProduct, singleProduct } from '../controllers/productControllers.js'
+import upload from '../middleware/multer.js'
+import adminAuth from '../middleware/adminAuth'
+
+const productRouter = express.Router()
+
+productRouter.post('/add', upload.single("image"), adminAuth, addProduct)
+
+productRouter.get('/list', listProducts)
+
+productRouter.post('/remove', adminAuth, removeProduct)
+
+productRouter.get('/single', singleProduct)
+
+export default productRouter
