a5f6a692a46869f51e82f7bb367a2e10af0e78a3
waw <anindyaagnes79@gmail.com>
2026-01-10 16:35:22 +0700

    homepage crash fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,20 @@
-import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 import HomeContent from "./components/HomeContent";
-import MerchCatalog from "./components/MerchCatalog";
+import MerchSlider from "./components/MerchSlider";
 import Footer from "./components/Footer";
+import {toast} from "react-toastify";
+
+
+export const backendUrl = 'http://localhost:4000'
 
 function App() {
-  const [page, setPage] = useState("home");
-
   return (
     <>
-      {/* Navbar */}
-      <Navbar setPage={setPage} />
-
-      {/* Konten utama */}
-      <div>
-        {page === "home" && (
-          <>
-            <HomeContent setPage={setPage} />
-          </>
-        )}
-
-        {page === "merch" && <MerchCatalog />}
-      </div>
-
-      {/* Footer */}
+      <Navbar />
+      <Banner />
+      <HomeContent />
+      <MerchSlider />
       <Footer />
     </>
   );
