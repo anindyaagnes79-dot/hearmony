644867ada312e5dc8e512abd85d3af39772e051b
waw <anindyaagnes79@gmail.com>
2026-01-11 12:14:49 +0700

    supaya bisa deploy

--- backend/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { adminLogin } from '../controllers/userControllers.js'
-
-const userRouter = express.Router();
-
-userRouter.post('/admin', adminLogin)
-
-export default userRouter
