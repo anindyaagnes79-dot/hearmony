366f11b97a6c62149197016752cc1697a441e77c
waw <anindyaagnes79@gmail.com>
2026-01-11 19:45:54 +0700

    rsvp fix fix

--- frontend/src/components/ReservationForm.jsx
+++ frontend/src/components/ReservationForm.jsx
@@ -1,13 +1,28 @@
+import { useEffect } from "react";
+
 function ReservationForm() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js";
+    script.async = true;
+    script.onload = () => {
+      window.jotformEmbedHandler(
+        "iframe[id='JotFormIFrame-260093924222452']",
+        "https://form.jotform.com/"
+      );
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <section className="min-h-screen bg-black text-white pt-[80px] pb-40">
-      <div className="max-w-5xl mx-auto min-h-[900px]">
+      <div className="max-w-5xl mx-auto">
         <iframe
+          id="JotFormIFrame-260093924222452"
           title="HEARMONY RESERVATION"
           src="https://form.jotform.com/260093924222452"
-          className="w-full min-h-[900px] border-none"
-          allow="geolocation; microphone; camera; fullscreen; payment"
-          scrolling="auto"
+          className="w-full border-none"
+          style={{ minHeight: "900px" }}
         />
       </div>
     </section>
