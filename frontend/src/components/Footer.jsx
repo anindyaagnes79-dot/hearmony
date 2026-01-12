66d8256aadb3f1cadfcb22781101a2f77d836b5c
fachritsaqif28 <fachri.tsaqif28@gmail.com>
2026-01-12 11:17:20 +0700

    Medsos

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,45 +1,65 @@
-import React from 'react'
-import logoFooter from '../assets/LOGO FOOTER.png'
+import React from "react";
+import logoFooter from "../assets/LOGO FOOTER.png";
 
 // LOGO SOSMED PNG
-import instagram from '../assets/instagram.png'
-import gmail from '../assets/gmail.png'
-import tiktok from '../assets/tiktok.png'
+import instagram from "../assets/instagram.png";
+import gmail from "../assets/gmail.png";
+import tiktok from "../assets/tiktok.png";
 
 function Footer() {
   return (
     <footer className="bg-black text-white py-12 px-8">
       <div className="max-w-7xl mx-auto flex flex-col items-center gap-6">
 
-        {/*LOGO HEARMONY TENGAH*/}
-        <img src={logoFooter} alt="Logo Footer" className="h-20 md:h-24 object-contain" />
+        {/* LOGO HEARMONY */}
+        <img
+          src={logoFooter}
+          alt="Logo Footer"
+          className="h-20 md:h-24 object-contain"
+        />
 
-        {/*LOGO SOSMED*/}
+        {/* SOSIAL MEDIA */}
         <div className="flex gap-8 justify-center">
-          <img
-            src={instagram}
-            alt="Instagram" href="https://www.instagram.com/hearmony_"
-            className="h-10 w-10 cursor-pointer hover:scale-110 transition-transform filter invert"
-          />
-          <img
-            src={tiktok}
-            alt="TikTok" href="https://www.tiktok.com/@hearmony_"
-            className="h-10 w-10 cursor-pointer hover:scale-110 transition-transform filter invert"
-          />
-          <img
-            src={gmail}
-            alt="Gmail" href="mailto:hearmonyofus@gmail.com"
-            className="h-10 w-10 cursor-pointer hover:scale-110 transition-transform filter invert"
-          />
+          <a
+            href="https://www.instagram.com/hearmony_?igsh=MXhua210ZjV5MXZicw=="
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={instagram}
+              alt="Instagram"
+              className="h-10 w-10 cursor-pointer hover:scale-110 transition-transform filter invert"
+            />
+          </a>
+
+          <a
+            href="https://www.tiktok.com/@hearmony_?_r=1&_t=ZS-9304B0VWty1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={tiktok}
+              alt="TikTok"
+              className="h-10 w-10 cursor-pointer hover:scale-110 transition-transform filter invert"
+            />
+          </a>
+
+          <a href="mailto:hearmonyofus@gmail.com">
+            <img
+              src={gmail}
+              alt="Email"
+              className="h-10 w-10 cursor-pointer hover:scale-110 transition-transform filter invert"
+            />
+          </a>
         </div>
 
-        {/* Copyright */}
+        {/* COPYRIGHT */}
         <p className="text-gray-500 text-sm text-center mt-6">
           © 2026 Aksang Cakru 31
         </p>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
