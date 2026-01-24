2e83e14679e82896817041f0aad5f23aafdf725c
waw <anindyaagnes79@gmail.com>
2026-01-24 20:14:40 +0700

    form rsvp kedua hore

--- frontend/src/components/ReservationForm.jsx
+++ frontend/src/components/ReservationForm.jsx
@@ -6,7 +6,7 @@ const ReservationForm = () => {
       <div className="w-full max-w-3xl">
         <iframe
           title="HEARMONY RESERVATION"
-          src="https://form.jotform.com/260093924222452"
+          src="https://form.jotform.com/260233355615048"
           className="w-full min-h-[900px] border-none block mx-auto"
           allow="geolocation; microphone; camera; fullscreen; payment"
           scrolling="auto"
